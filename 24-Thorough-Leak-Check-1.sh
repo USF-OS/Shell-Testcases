1d1fd903d86f7b0062c8b6f9e7fa96eb9d5acacf
Matthew Malensek <matt@malensek.net>
2020-10-19 15:09:42 -0700

    Add FD checking

--- 24-Thorough-Leak-Check-1.sh
+++ 24-Thorough-Leak-Check-1.sh
@@ -2,23 +2,35 @@ source "${TEST_DIR}/lib/funcs.bash"
 
 run_timeout=30
 
+fd_check='
+{
+    if (/==[0-9]*==\s*Open file descriptor/) {
+        fd=$0;
+        getline;
+        if (! /inherited/) {
+            print fd
+        }
+    }
+}' 
+
 test_start "Thorough Memory Leak Check"
 
-valgrind_output=$(valgrind \
+program_output=$(valgrind \
     --trace-children=no \
     --child-silent-after-fork=yes \
     --leak-check=full \
+    --track-fds=yes \
+    --track-origins=yes \
     ./$SHELL_NAME < "${TEST_DIR}/inputs/scripts/leak.sh" 2>&1)
+echo "${program_output}"
 
-grep 'are definitely lost' <<< "${valgrind_output}"
+# Check for open FDs
+awk "${fd_check}" <<< "${program_output}" \
+    | grep -i '==[0-9]*==.*file descriptor' && test_end 1
 
-if [[ $? -eq 0 ]]; then
-    echo "Leaks detected! Showing valgrind output."
+# Make sure there were no blocks definitely lost
+grep -i --after-context=1 '==[0-9]*==\s*definitely lost' \
+    <<< "${program_output}" && test_end 1
 
-    echo "${valgrind_output}"
-
-    test_end 1
-else
-    test_end 0
-fi
-
+# If none of the conditions were triggered above, the test passes.
+test_end 0 
