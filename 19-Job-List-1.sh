1b923a46ff8af74256486a462a741b04a2bd5842
Matthew Malensek <matt@malensek.net>
2020-10-19 15:29:42 -0700

    Fix job list hang

--- 19-Job-List-1.sh
+++ 19-Job-List-1.sh
@@ -2,24 +2,22 @@ source "${TEST_DIR}/lib/funcs.bash"
 
 run_timeout=5
 
-trap '# Interrupt' INT
-
 script=$(cat <<EOM
-sleep 1 &
-sleep 2 &
-sleep 500 &
-sleep 100 &
-sleep 450 &
-sleep 3
+${TEST_DIR}/inputs/scripts/sleeper.sh 1 &
+${TEST_DIR}/inputs/scripts/sleeper.sh 2 &
+${TEST_DIR}/inputs/scripts/sleeper.sh 500 &
+${TEST_DIR}/inputs/scripts/sleeper.sh 100 &
+${TEST_DIR}/inputs/scripts/sleeper.sh 450 &
+${TEST_DIR}/inputs/scripts/sleeper.sh 3
 jobs
 ${TEST_DIR}/inputs/scripts/kill-parent.sh
 EOM
 )
 
 expected=$(cat <<EOM
-sleep 100
-sleep 450
-sleep 500
+sleeper.sh 100
+sleeper.sh 450
+sleeper.sh 500
 EOM
 )
 
@@ -31,12 +29,15 @@ echo "${script}"
 
 run ./$SHELL_NAME < <(echo "${script}")
 
+# We process the output before displaying it here.
 # We ignore:
 # - pids (unique on each system)
 # - & (optional to display in the job list)
-processed_output=$(grep -o 'sleep\s*\([0-9]*\)' <<< "${program_output}" \
+processed_output=$(grep -o 'sleeper.sh\s*\([0-9]*\)' <<< "${program_output}" \
     | sort -n)
 
+# Don't forget to check the comments above to understand how
+# this output is formatted!
 compare <(echo "${expected}") <(echo "${processed_output}")
 
 test_end
