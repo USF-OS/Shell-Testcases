98215f7cc017ea185af7d43b0b142b6ad8dbcf94
Matthew Malensek <matt@malensek.net>
2020-10-19 15:18:26 -0700

    Add input and append to test 17

--- 17-Pipes-&-Redirection-1.sh
+++ 17-Pipes-&-Redirection-1.sh
@@ -3,9 +3,14 @@ source "${TEST_DIR}/lib/funcs.bash"
 run_timeout=5
 
 script=$(cat <<EOM
+rm -f ${TEST_DIR}/mega-output.txt
+rm -f ${TEST_DIR}/mega-output2.txt
 ls -1 /bin | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n-1 | tail -n+2 | head -n 10 | tail -n 5 > ${TEST_DIR}/mega-output.txt
+sort -r < ${TEST_DIR}/mega-output.txt > ${TEST_DIR}/mega-output2.txt
+cat ${TEST_DIR}/mega-output2.txt >> ${TEST_DIR}/mega-output.txt
 cat ${TEST_DIR}/mega-output.txt
-rm ${TEST_DIR}/mega-output.txt
+rm -f ${TEST_DIR}/mega-output.txt
+rm -f ${TEST_DIR}/mega-output2.txt
 EOM
 )
 
